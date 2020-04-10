0e5978e0b2aafdf8a6523e678a406d893ee2f1a7
vikas-nichi <vikas.ep@nichi.com>
2020-04-10 18:44:41 +0530

    adding a variable

--- src/Greeting.svelte
+++ src/Greeting.svelte
@@ -1,9 +1,13 @@
+<script>
+  export let name = "again";
+</script>
+
 <style>
   h2 {
-    color: aqua;
+    color: purple;
     font-family: "Roboto";
     font-size: 2em;
   }
 </style>
 
-<h2>Hello Vikas</h2>
+<h2>Hello Vikas {name}</h2>
